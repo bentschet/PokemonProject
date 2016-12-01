b76bd52ceab2a5066b0b5d501f4eb5b48101518a
Benjamin Tschetter <benjamintschetter3@gmail.com>
2016-12-01 13:17:20 -0700

    populated the constructor of PokemonPanel.java
    Lots of labels.

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,6 +1,30 @@
 package pokemon.view;
 
-public class PokemonPanel
-{
+import java.awt.Color;
+import javax.swing.*;
+import pokemon.controller.PokemonController;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class PokemonPanel extends JPanel
+{
+	private PokemonController baseController;
+	private ImageIcon pokemonIcon;
+	private SpringLayout baseLayout;
+	private JButton updateButton;
+	private JComboBox pokedexSelector;
+	private JLabel pokemonLabel;
+	private JLabel healthLabel;
+	private JLabel combatLabel;
+	private JLabel speedLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel advancedLabel;
+	private JTextField healthField;
+	private JTextField combatField;
+	private JTextField speedField;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextArea advancedArea;
 }
