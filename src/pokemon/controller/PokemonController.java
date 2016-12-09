2be054aab0d7d9a33615428fae7271781cc1cd7c
bentschet <btsc0323@740s-pl-126.local>
2016-12-09 13:33:00 -0700

    imported mouseMotionListener and MouseListener

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,9 +1,16 @@
 package pokemon.controller;
 
+import java.awt.Component;
+
 public class PokemonController
 {
 	public void start()
 	{
 		
+	}
+
+	public Component getBaseFrame() {
+		
+		return null;
 	}
 }
--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -4,8 +4,8 @@ import java.awt.Color;
 import javax.swing.*;
 import pokemon.controller.PokemonController;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.awt.event.MouseMotionListener;
 
 public class PokemonPanel extends JPanel
 {
@@ -37,5 +37,53 @@ public class PokemonPanel extends JPanel
 		numberField.setEditable(false);
 		advancedArea.setEditable(false);
 		advancedArea.setWrapStyleWord(true);
+	}
+	
+	private void setupListeners()
+	{
+		this.addMouseListener(new MouseListener()
+				{
+					public void mouseEntered(MouseEvent entered)
+					{
+					//	JOptionPane.showMessageDialog(baseController.getBaseFrame(), "The mouse entered the program");
+					}
+					
+					public void mouseReleased(MouseEvent released)
+					{
+					//	System.out.println("released");
+					}
+					
+					public void mouseExited(MouseEvent exited)
+					{
+					//	JOptionPane.showMessageDialog(baseController.getBaseFrame(), "The mouse exited the program");
+					}
+					
+					public void mouseClicked(MouseEvent clicked)
+					{
+					//	JOptionPane.showMessageDialog(baseController.getBaseFrame(), "The mouse was clicked");
+					}
+					
+					public void mousePressed(MouseEvent pressed)
+					{
+                   	//	System.out.println("pressed");
+					}
+				});
+		this.addMouseMotionListener(new MouseMotionListener()
+				{
+					public void mouseMoved(MouseEvent moved)
+					{
+						if(((Math.abs(moved.getX() - updateButton.getX()) < 5 ) || (Math.abs(moved.getY() - updateButton.getY() <5)))
+						{
+							updateButton.setLocation(moved.getX() + 10, moved.getY() - 10);
+							
+						}
+						//	JOptionPane.showMessageDialog(baseController.getBaseFrame(), "The mouse was at X:" + moved.getX() +", Y:" + moved.getY());
+					}
+					
+					public void mouseDragged(MouseEvent dragged)
+					{
+					//	setRandomColor();
+					}
+				});
 	}
 }
